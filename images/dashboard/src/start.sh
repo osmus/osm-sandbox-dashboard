03a02655342b63a17c11ef4eb8d1bf7fb2e8bcab
Rub21 <rub2106@gmail.com>
2024-07-24 10:28:20 -0500

    Remove pg checker in start.sh

--- images/dashboard/src/start.sh
+++ images/dashboard/src/start.sh
@@ -1,28 +1,3 @@
 #!/usr/bin/env bash
-function check_db {
-    python - <<END
-import psycopg2
-import sys
-import os
-try:
-    conn = psycopg2.connect(
-        dbname=os.getenv('POSTGRES_DB'), 
-        user=os.getenv('POSTGRES_USER'), 
-        password=os.getenv('POSTGRES_PASSWORD'), 
-        host=os.getenv('POSTGRES_HOST')
-    )
-except psycopg2.OperationalError:
-    sys.exit(1)
-sys.exit(0)
-END
-}
-
-# Loop until the database is ready
-until check_db; do
-    echo "Waiting for the database to be ready..."
-    sleep 2
-done
-
-# Start app
 python init_db.py
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload
