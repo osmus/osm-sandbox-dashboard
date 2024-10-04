7825c72d3351f5ec2b1e9f823dce01234f55ec2e
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2024-10-04 01:06:04 -0400

    Adjust order

--- images/dashboard/src/models/sessions.py
+++ images/dashboard/src/models/sessions.py
@@ -7,6 +7,6 @@ class Sessions(Base):
     __tablename__ = "sessions"
     id = Column(String, primary_key=True, index=True)
     box = Column(String)
-    end_redirect_uri = Column(String, nullable=True)
     user = Column(String, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
+    end_redirect_uri = Column(String, nullable=True)
